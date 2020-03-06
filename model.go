5a510c18d830ab6769290e01fb108e26e6eace4e
doudou <50986402+doudou1016@users.noreply.github.com>
2020-03-06 23:19:16 +0800

    Update model.go

--- model.go
+++ model.go
@@ -6,7 +6,7 @@ type Model struct {
 	ID        int        `gorm:"primary_key;auto_increment" json:"id"`
 	CreatedAt time.Time  `gorm:"column:created_at;type:datetime;" json:"created_at"` // 创建时间
 	UpdatedAt time.Time  `gorm:"column:updated_at;type:datetime;" json:"updated_at"` // 更新时间
-	DeletedAt *time.Time `gorm:"column:updated_at;type:datetime;" json:"deleted_at" sql:"index"`
+	DeletedAt *time.Time `gorm:"column:deleted_at;type:datetime;" json:"deleted_at" sql:"index"`
 }
 
 type ModelID struct {
